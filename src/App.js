7bda37c5ed7b6d89813dcf5c957922047a1f7127
Chelo <chelorb@hotmail.com>
2023-12-02 19:47:29 -0300

    comit1

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+function App() {
+  return (
+    <div className='container'>
+      <div class="alert alert-secondary text-center" role="alert">
+        <h2>
+          Página 1 - INICIO
+        </h2>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Componentes/Navbar/index.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+
+function Navbar() {
+
+    return (
+        
+        <div className="shadow-lg ">
+            <ul className="list-inline">
+                <li className="e">
+                    <a href="" className="">Zapatos</a>
+                </li>
+                <li className="">
+                    <a href="" className="">Carteras</a>
+                </li>
+                <li className="">
+                    <a href="" className="">Ofertas</a>
+                </li>
+                <li className="">
+                    <a href="" className="">Contacto</a>
+                </li>
+                <li className="">
+                    <a href="" className="">Redes</a>
+                </li>
+            </ul>
+            
+          
+            
+        </div>
+        
+
+    )
+}
+
+export default Navbar;
